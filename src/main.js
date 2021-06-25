4be361a511abad7b6453d46b4036f835e33f5478
Scott Gall <scottarthurgall@gmail.com>
2021-06-24 21:47:23 -0500

    import authhandler in app

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App';
 import store from './store';
+import AuthHandler from './components/AuthHandler';
 
 Vue.use(VueRouter);
 
