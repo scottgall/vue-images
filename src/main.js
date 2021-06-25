e5e640179817dce7c90e1832bf849401b6f27eed
Scott Gall <scottarthurgall@gmail.com>
2021-06-24 21:42:15 -0500

    vue use vuerouter

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import VueRouter from 'vue-router';
 import App from './App';
 import store from './store';
 
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  
+});
+
 new Vue({
   store,
   render: h => h(App)
